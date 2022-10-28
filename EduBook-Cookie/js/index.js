ee9b501ea67ab0e7d0c733c398abda092fb8bc81
Loktrbl <conca8048@gmail.com>
2022-10-28 18:58:28 +0700

    Chỉnh sửa Css, Index

--- EduBook-Cookie/js/index.js
+++ EduBook-Cookie/js/index.js
@@ -1,15 +1,15 @@
 
 
 
-setTimeout(function(){
-    location.reload();
-}, 60* 1000);
+// setTimeout(function(){
+//     location.reload();
+// }, 60* 1000);
 
 //Get the button
 var mybutton = document.getElementById("myBtn-scroll");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
